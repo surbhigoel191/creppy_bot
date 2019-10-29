612381cc294aa7a24d4979d9657806eb3b7ace82
surbhigoel191 <44471561+surbhigoel191@users.noreply.github.com>
2019-10-29 11:56:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create a new Discord client
+const client = new Discord.Client();
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content.toLowerCase() === 'ping') {
+    message.channel.send('pong!');
+  } 
+});
+
+// Log our bot in using the token
+client.login('NjM2NTUyODMxNDU2OTAzMTc5.XbBTWA.rnwXQNZjS0ql0ZrWgTAc3-qwvlQ');
